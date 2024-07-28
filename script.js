316088ed92a30b05743ac8a8f7bd7626314cd1c9
Focusmyme <146586311+Focusmyme@users.noreply.github.com>
2024-07-27 23:44:01 -0300

    adição de novas áreas e novas informações

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function aparece() {
 
 let link = window.document.getElementById('link')
-let setab = window.document.getElementById('setab')
+let seta = window.document.getElementById('setab')
 
-if(link.style.top === '-38%') {link.style.top = '88%'
-setab.src = 'imagens/seta-para-cima.png'
+if(link.style.top === '-38%' || link.style.top === '') {link.style.top = '88%'
+seta.src = 'imagens/seta-para-cima.png'
     link.style.transition = 'top 1s ease'
 } else {link.style.top = '-38%'
-    setab.src = 'imagens/seta-para-baixo-2.png'
+    seta.src = 'imagens/seta-para-baixo-2.png'
 }
 }
